92647a17a68f184ffe2b60f6b2bc1cb4218c8065
miles witt <mileswitt0903@gmail.com>
2026-05-22 15:19:18 -0700

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> st;
+
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
+        for (int i = 0; i<s.length(); i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                st.push(s[i]);
+            }
+
+            if (s[i] == ')' ) {
+                if (st.empty() || st.top() != '(') {
+                    return false;
+                } 
+                st.pop();
+            }
+            if (s[i] == ']' ) {
+                if (st.empty() || st.top() != '[') {
+                    return false;
+                } 
+                st.pop();
+            }
+            if (s[i] == '}' ) {
+                if (st.empty() || st.top() != '{') {
+                    return false;
+                } 
+                st.pop();
+            }
+
+        }
+
+        return st.empty();
+        
+    }
+};
