a528438e691a5b2b14bdb4fdb2431cb16fa590bd
miles witt <mileswitt0903@gmail.com>
2026-06-06 15:02:14 -0700

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+
+        priority_queue<int> maxHeap(stones.begin(), stones.end());
+
+        while (maxHeap.size() > 1) {
+            int stone1 = maxHeap.top();
+            maxHeap.pop();
+            int stone2 = maxHeap.top();
+            maxHeap.pop();
+            int res = stone1 - stone2;
+
+            maxHeap.push(res);
+        }
+        return maxHeap.top();
+    }
+};
